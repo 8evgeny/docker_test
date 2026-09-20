d905c5a537654afc9407b4cc15fa3cfa123e8073
Evgeny Parubets <8evgeny@gmail.com>
2026-09-20 14:33:03 +0300

    qt attempt

--- qtcreator/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bash -c './start.sh>/dev/null 2>&1'
--- qtcreator/start.sh
+++ qtcreator/start.sh
@@ -2,15 +2,31 @@
 
 xhost +
 
-docker run --rm -e DISPLAY=$DISPLAY \
--v /tmp/.X11-unix:/tmp/.X11-unix  \
--v /tmp/.docker.Xauthority -e XAUTHORITY=/tmp/.docker.Xauthority \
---net=host \
---privileged \
---name qtcreator \
---cgroupns host \
---cap-add=ALL \
-qtcreator:latest
+
+
+docker run -it --rm \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+  -v $(pwd):/app \
+  --name qtcreator \
+  qtcreator-container:latest \
+  bash
+
+# Собралось нет исполняемого файла
+
+#  /opt/qtcreator/bin/qtcreator
+
+
+
+#docker run --rm -e DISPLAY=$DISPLAY \
+#-v /tmp/.X11-unix:/tmp/.X11-unix  \
+#-v /tmp/.docker.Xauthority -e XAUTHORITY=/tmp/.docker.Xauthority \
+#--net=host \
+#--privileged \
+#--name qtcreator \
+#--cgroupns host \
+#--cap-add=ALL \
+#qtcreator:latest
 
 #--user user:user \
 #-v "/home/user/QtProg/:/home/user/QtProg" \
